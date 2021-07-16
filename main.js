2a7732d0b80fd9e9a239d07200cb77d58452b47b
RenanRibeir <renanribeirow19@gmail.com>
2021-07-15 22:27:23 -0300

    Adicionados preços e implementação de valor total de pedido

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var text = document.getElementById("pedido")
 var idItem = 0;
+var total = 0;
 
 function enviar()
 {
@@ -7,57 +8,71 @@ function enviar()
     console.log(filhos)
     var msg = 'Pedido%20de%20Carlos%20Mesa%2001%0A';
     
-    var phone = 0000000000
+    var phone = 558597821541
     for(var i = 0;i<filhos.length;i++)
-     msg += filhos[i].innerText.substring(0,filhos[i].innerText.length - 7) + "%0A"
+     msg += filhos[i].innerText.substring(0,filhos[i].innerText.length - 1) + "%0A"
     
+    msg += 'Total%20R$%20'+total
     console.log(msg)
     window.open("https://api.whatsapp.com/send?phone=" + phone + "&text=" + msg, "_blank");
 }
 
-function adicionaritemSimples(item)
+function adicionaritemSimples(item,valor)
 {
-
-    sessionStorage[idItem]  = '<div class="item"  id="'+idItem+'"><text> - '+item+'</text><text onclick="remover('+idItem+')">X</text></div>'
-    text.innerHTML =  text.innerHTML + sessionStorage[idItem];
+    item += ' - '+valor;
+    sessionStorage[idItem]  = valor
+    text.innerHTML =  text.innerHTML +'<div class="item"  id="'+idItem+'"><text> - '+item+'</text><text onclick="remover('+idItem+')">X</text></div>';
 
     idItem++;
+    total += valor
+    
+    document.getElementById('totalPedido').innerHTML = 'Total: R$ '+ total;
 }
 
-function adicionaritem(item,cx)
+function adicionaritem(item,cx,valor)
 {
 
     var select = document.getElementById(cx);
 	var value = select.options[select.selectedIndex].value;
-
-    sessionStorage[idItem]  = '<div class="item"  id="'+idItem+'"><text> - '+item+' '+value+'</text><text onclick="remover('+idItem+')">X</text></div>'
+    item += ' '+value+' - '+valor;
+    sessionStorage[idItem]  = valor
     
-    text.innerHTML =  text.innerHTML + sessionStorage[idItem];
+    text.innerHTML =  text.innerHTML +'<div class="item"  id="'+idItem+'"><text> - '+item+'</text><text onclick="remover('+idItem+')">X</text></div>';
 
     idItem++;
+    
+    total += valor
+    
+    document.getElementById('totalPedido').innerHTML = 'Total: R$ '+ total;
 }
 
-function adicionarFoudue(item)
+function adicionarFoudue(item,valor)
 {
 
     if(document.getElementById('adicional').checked){
         item += " com Mashmellow"; 
+        valor += 2;
     }
-
-    sessionStorage[idItem] = '<div class="item"  id="'+idItem+'"><text> - '+item+'</text><text onclick="remover('+idItem+')">X</text></div>'
+    item += ' - '+valor
+    sessionStorage[idItem] = valor
     
-    text.innerHTML =  text.innerHTML + sessionStorage[idItem];
+    text.innerHTML =  text.innerHTML + '<div class="item"  id="'+idItem+'"><text> - '+item+'</text><text onclick="remover('+idItem+')">X</text></div>';
 
     idItem++;
-
+    
+    total += valor
+    document.getElementById('totalPedido').innerHTML = 'Total: R$ '+ total;
 }
 
 function remover(id)
 {
 
     var node = document.getElementById(id);
+    total -= sessionStorage[id]
     if (node.parentNode) {
         node.parentNode.removeChild(node);
     }
+    
+    document.getElementById('totalPedido').innerHTML = 'Total: R$ '+ total;
 
 }
